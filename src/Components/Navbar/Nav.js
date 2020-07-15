8d4e0f95e7186f7e4d574b5e5e059e1e6aa241ac
vrushit <bharuchavrushit@ymail.com>
2020-07-15 11:31:41 +0530

    Final Commit

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <nav className="nav">
+      <Link className="navStyle" to="/">
         <h3>Logo</h3>
+        </Link>
         <ul className="nav-links">
+          <Link className="navStyle" to="/about">
           <li>About</li>
+          </Link>
+          <Link className="navStyle" to="/shop">
           <li>Shop</li>
-
+          </Link>
         </ul>
 
     </nav>
